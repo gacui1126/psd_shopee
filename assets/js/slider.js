e6b8b3bda109292f4d3c9b4e956dd1e14046133b
Nguyễn Trường Sơn <nguyentruongson@MacBook-Pro-cua-Son.local>
2022-03-11 12:30:00 +0700

    “first”

---
+++ assets/js/slider.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+showSlides();
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" span-focus", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " span-focus";
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 3000); 
+}
+
+
---
+++ assets/js/text.js
@@ -0,0 +1,7 @@
+const main = document.querySelector(".home-page__banner-main");
+const link = document.querySelector(".home-page__banner-main-link");
+const img = document.querySelectorAll(".mySlides");
+const imgWidth = img[0].offsetWidth;
+const sliderLength = img.length;
+
+console.log(sliderLength);
